b2ca959a4797f9263854a78cdf5731be161096ca
saungwaikhin <saungwai.htd33333@gmail.com>
2025-06-18 10:34:43 +0630

    dashboard design fix

---
+++ layouts/footer.php
@@ -0,0 +1,57 @@
+<!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+            <div class="copyright">
+                <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a> 2018</p>
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
+    </div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <script src="../dashJs/bootstrap-datepicker.min.js"></script>
+    <script src="../dashJs/bootstrap-material-datetimepicker.js"></script>
+    <script src="../dashJs/bootstrap-timepicker.min.js"></script>
+    <script src="../dashJs/common.min.js"></script>
+    <script src="../dashJs/custom.min.js"></script>
+    <script src="../dashJs/daterangepicker.js"></script>
+    <script src="../dashJs/form-pickers-init.js"></script>
+    <!-- Clock Plugin JavaScript -->
+    <script src="../dashJs/fullcalendar-init.js"></script>
+    <!-- Color Picker Plugin JavaScript -->
+    <script src="../dashJs/fullcalendar.min.js"></script>
+    <script src="../dashJs/gleek.js"></script>
+    <script src="../dashJs/jquery-asColor.js"></script>
+    <!-- Date Picker Plugin JavaScript -->
+    <script src="../dashJs/jquery-asColorPicker.min.js"></script>
+    <!-- Date range Plugin JavaScript -->
+    <script src="../dashJs/jquery-asGradient.js"></script>
+    <script src="../dashJs/jquery-clockpicker.min.js"></script>
+
+    <script src="../dashJs/jquery-ui.min.js"></script>
+    <script>
+        setTimeout(function() {
+        $('.alert').hide();
+        }, 3000)
+    </script>
+
+    
+    <script src="../dashJs/jquery.min.js"></script>
+    <script src="../dashJs/moment.js"></script>
+    <script src="../dashJs/moment.min.js"></script>
+    <script src="../dashJs/settings.js"></script>
+    <script src="../dashJs/styleSwitcher.js"></script>
+    <script src="../dashJs/sweetalert2.all.min.js"></script>
+
+
+</body>
+
+</html>
